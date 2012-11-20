e1a4369d0a13736457b75fd495d94587453fbaf0
Eugen Rochko <gargron@gmail.com>
2012-11-20 20:56:09 +0100

    System-related message specs

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -2,11 +2,35 @@ require 'spec_helper'
 
 describe Chat::Message do
   context "system" do
-    let(:message) { Chat::Message.new }
+    let(:message) { Chat::Message.new { :type => :system, :text => "Hello" } }
 
     describe "#new" do
       it "returns a Message object" do
         message.should be_an_instance_of Chat::Message
+      end
+    end
+
+    describe "#type" do
+      it "returns the type of the message" do
+        message.type.should eql :system
+      end
+    end
+
+    describe "#text" do
+      it "returns the text of the message" do
+        message.text.should eql "Hello"
+      end
+    end
+
+    describe "#to_json" do
+      it "returns the JSON string of the message" do
+        message.to_json.should eql "{type:\"system\",text:\"Hello\"}"
+      end
+    end
+
+    describe "#public?" do
+      it "returns whether the message is to be broadcast or sent to one client" do
+        message.public?.should be_true
       end
     end
   end
