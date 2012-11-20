b1f8b5e6eed4931aeb35df0452a8615848d1948e
Eugen Rochko <gargron@gmail.com>
2012-11-20 21:46:30 +0100

    Adding #initialize and #public? to Message

--- chat/message.rb
+++ chat/message.rb
@@ -1,4 +1,31 @@
 module Chat
   class Message
+    # Public: Returns type of message
+    attr_reader :type
+
+    # Public: Returns content of message
+    attr_reader :text
+
+    # Public: Returns owner of message
+    attr_reader :user
+
+    # Public: Initializes a new message instance
+    #
+    # options - Hash options used to instantiate the message polymorphically
+    #           :type - Type of the message
+    #           :user - Owner of the message (optional)
+    #           :text - The contents of the message
+    def initialize(options)
+      @type = options[:type]
+      @text = options[:text]
+      @user = options[:user]
+    end
+
+    # Public: Is the message for broadcast or for one client?
+    #
+    # Returns the boolean value
+    def public?
+      [:system, :text].include? @type
+    end
   end
 end
