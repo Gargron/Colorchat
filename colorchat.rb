5d0355c2a4fce0ddd4d2d5e2d588628ea0e33c0c
Eugen Rochko <gargron@gmail.com>
2012-11-21 19:11:50 +0100

    Host bind fix

--- colorchat.rb
+++ colorchat.rb
@@ -12,7 +12,7 @@ require_relative './chat/message'
 require_relative './protocols/websockets/chatconnection'
 
 EventMachine.run do
-  EventMachine::WebSocket.start(:host => '127.0.0.1', :port => 8050) do |socket|
+  EventMachine::WebSocket.start(:host => '0.0.0.0', :port => 8050) do |socket|
     connection = Protocols::WebSockets::ChatConnection.new socket
 
     socket.onopen    {        connection.onopen }
