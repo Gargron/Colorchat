96f9da3251a6c339df8a422dc88c7642ff563d02
Eugen Rochko <gargron@gmail.com>
2012-12-05 09:02:06 +0100

    Test require sequence

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,3 @@
-require_relative '../chat/user'
-require_relative '../chat/command'
-require_relative '../chat/message'
-require_relative '../chat/room'
-
 require 'yaml'
 require 'mocha/setup'
 require 'redis'
@@ -10,6 +5,11 @@ require 'mock_redis'
 require 'json'
 require 'eventmachine'
 require 'digest/md5'
+
+require_relative '../chat/user'
+require_relative '../chat/command'
+require_relative '../chat/message'
+require_relative '../chat/room'
 
 RSpec.configure do |config|
   config.before :all do
