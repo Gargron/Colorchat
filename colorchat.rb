fdac3d85cbf38d48cce71c9564673f6087877806
Eugen Rochko <gargron@gmail.com>
2012-12-25 20:43:48 +0100

    Slightly more logging

--- colorchat.rb
+++ colorchat.rb
@@ -9,9 +9,11 @@ Dir["#{File.dirname(__FILE__)}/chat/*.rb"].each        { |file| require file }
 Dir["#{File.dirname(__FILE__)}/protocols/*/*.rb"].each { |file| require file }
 
 EventMachine.run do
+  puts "Starting Colorchat..."
   chat = Chat::Main.new
 
   EventMachine::WebSocket.start(:host => '0.0.0.0', :port => (ARGV[0] || 8050)) do |socket|
+    puts "WebSocket connection opened"
     connection = Protocols::WebSockets::ChatConnection.new socket, chat
 
     socket.onopen    {        connection.onopen }
