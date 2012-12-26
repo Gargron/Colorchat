823c4a895ce9caeb38338d9d29e0998d973654b0
Eugen Rochko <gargron@gmail.com>
2012-12-26 14:50:59 +0100

    Adding examples

---
+++ public/example.auth.php
@@ -0,0 +1,55 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+  if(!class_exists('Redis')) {
+    throw new Exception("Please install the PHPRedis extension");
+  }
+
+  $name  = trim($_POST['name']);
+  $email = trim($_POST['email']);
+  $color = trim($_POST['color']);
+  $nsfw  = trim($_POST['nsfw']);
+
+  if(empty($name) || empty($email) || empty($color) || empty($nsfw)) {
+    throw new Exception("Invalid input, this is an example script, please play by rules");
+  }
+
+  $redis = new Redis();
+  $redis->connect();
+
+  $newID      = $redis->incr('user:ids');
+  $identifier = md5(uniqid());
+
+  $user  = array(
+
+    'id'    => (int) $newID,
+    'name'  => $name,
+    'email' => $email,
+    'role'  => 1,
+    'color' => $color,
+    'nsfw'  => ($nsfw === 'true'),
+
+  );
+
+  $redis->setex('user:session:' . $identifier, 3600 * 6, json_encode($user));
+  setcookie('auth', $identifier, time() + (3600 * 6));
+}
+
+?>
+
+<!DOCTYPE html>
+
+<?php if(isset($user)): ?>
+  <strong>User created, cookie set. Go to the chat now!</strong>
+<?php endif; ?>
+
+<form action="" method="post">
+  <label>Name: <input name="name"></label><br />
+  <label>E-mail: <input name="email"></label><br />
+  <label>HEX color: <input name="color"></label><br />
+
+  <label><input type="radio" name="nsfw" value="true"> Show NSFW messages</label>
+  <label><input type="radio" name="nsfw" value="false"> Hide NSFW messages</label>
+
+  <button type="submit">Sign in</button>
+</form>
---
+++ public/example.index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf8">
+    <title>Example chat client</title>
+  </head>
+
+  <body>
+    <label>Message: <input id="message" placeholder="Press ENTER to send"></label>
+
+    <div id="messages"></div>
+
+    <script type="text/javascript">
+      window.onload = function () {
+        const WS_URI = 'example.com:8050';
+        const AUTH   = '<?php echo $_COOKIE["auth"]; ?>';
+
+        var input = document.getElementById('message'),
+          output  = document.getElementById('messages'),
+          client  = new WebSocket(WS_URI);
+
+        if (typeof JSON === "undefined") {
+          alert('JSON parsing not supported in browser, include json2.js or whatever in this example page');
+          return;
+        }
+
+        input.onkeyup = function (evt) {
+          if (evt.keyCode === 13 && client.readyState === 1) {
+            client.send(input.value);
+            input.value = '';
+          }
+        };
+
+        client.onopen = function (evt) {
+          alert('Connected');
+        };
+
+        client.onclose = function (evt) {
+          alert('Disconnected');
+        };
+
+        client.onmessage = function (evt) {
+          var data = JSON.parse(evt.data);
+          output.innerHTML += '<div>' + data.user.name + ': ' + data.text + '</div>';
+        };
+
+        client.onerror = function (evt) {
+          alert('Error');
+        };
+      };
+    </script>
+  </body>
+</html>
