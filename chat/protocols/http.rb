36964c96f95ae3f7eec557b7a36e3dff17301faf
Eugen Rochko <gargron@gmail.com>
2013-01-07 09:45:51 +0100

    Actually Comet, but won't do anything yet

--- chat/protocols/http.rb
+++ chat/protocols/http.rb
@@ -5,6 +5,7 @@ module Chat
 
       def process_http_request
         res = EventMachine::DelegatedHttpResponse.new self
+        new_message = false
 
         puts "[#{Time.now}] Comet: request_method : #{@http_request_method}"
         puts "[#{Time.now}] Comet: path_info : #{@http_path_info}"
@@ -12,11 +13,17 @@ module Chat
         puts "[#{Time.now}] Comet: post_content : #{@http_post_content}"
 
         if @http_request_method == 'POST'
+          new_message = true
           res.status  = 200
           res.content = 'OK'
           res.send_response
         elsif @http_request_method == 'GET'
           EventMachine::defer do
+            60.times do
+              break if new_message
+              sleep 1
+            end
+
             res.status  = 200
             res.content = 'Poop'
             res.send_response
