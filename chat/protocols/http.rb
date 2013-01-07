7e22b48debca6601bd47d9535b54db9993836f30
Eugen Rochko <gargron@gmail.com>
2013-01-07 09:21:26 +0100

    Adding some simple debugging

--- chat/protocols/http.rb
+++ chat/protocols/http.rb
@@ -4,7 +4,12 @@ module Chat
       include EventMachine::HttpServer
 
       def process_http_request
-        # TODO
+        res = EventMachine::DelegatedHttpResponse.new self
+
+        puts "[#{Time.now}] Comet: request_method : #{@http_request_method}"
+        puts "[#{Time.now}] Comet: path_info : #{@http_path_info}"
+        puts "[#{Time.now}] Comet: query_str : #{@http_query_string}"
+        puts "[#{Time.now}] Comet: post_content : #{@http_post_content}"
       end
     end
   end
